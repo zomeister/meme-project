b3d60dd0ddd8764990e1394d01dd7ad22034cedb
Ann McElderry <thayerann1@gmail.com>
2023-04-03 14:16:22 -0500

    reverted to previous state

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
-function fetchMemes() {
-    fetch ('http://localhost:3000/memes')
-    .then (res => res.json())
-    .then (console.log)
+
+function renderMeme(emotion) {
+    return
 }
 
-fetchMemes();
+function getMeme(emotion) {
+    return
+}
 
-// function renderMeme(emotion) {
-//     return
-// }
+function addLike() {
+    return
+}
 
-// function getMeme(emotion) {
-//     return
-// }
-
-// function addLike() {
-//     return
-// }
-
-// function removeLike() {
-//     return
-// }
+function removeLike() {
+    return
+}
 
