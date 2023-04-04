96950939b4f7d6805e2e127976ac5c1ef20ec0a7
Ann McElderry <thayerann1@gmail.com>
2023-04-04 14:07:40 -0500

    updated files

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
+const baseUrl = "http://localhost:3000/"
+const feelingsUrl = baseUrl + "feelings/"
 
-function renderMeme(emotion) {
-    return
+function fetchFeelings() {
+    fetch(feelingsUrl)
+    .then(res => res.json())
+    .then(feelingsData => renderAllFeelings(feelingsData))
 }
 
-function getMeme(emotion) {
-    return
+fetchFeelings();
+
+function renderAllFeelings(feelingsData) {
+    feelingsData.forEach(feeling => renderFeeling(feeling))
 }
 
-function addLike() {
-    return
+
+function renderFeeling(feeling) {
+    
+    const emotionlistNav = document.getElementById('emotions-list')
+    const emotionButton = document.createElement('button')
+    emotionlistNav.appendChild(emotionButton)
+
+    emotionButton.id = feeling.id
+    emotionButton.className = "hover-div"
+    emotionButton.textContent = feeling.name
+    // emotionButton.innerHTML = `<img src=${feeling.emoji}>`
+
+    emotionButton.addEventListener('click', (e) => displayMemesByFeeling (feeling))
 }
 
-function removeLike() {
-    return
-}
+function displayMemesByFeeling(feeling) {
+    
+    const memeImages = document.getElementsByClassName('meme-images')
+    const memeLikes = document.getElementsByClassName("control-label")
 
+    let i = 0
+    
+    for (let meme of memeImages) {
+        meme.src = feeling.memes[i++].imageUrl
+    }
+
+    i = 0
+
+    for (let label of memeLikes) {
+        label.textContent = feeling.memes[i++].likes + " Likes"
+    }
+
+    const likeButtons = document.getElementsByClassName('btn')
+
+    for (const btn of likeButtons) {
+        btn.value = "♥"
+    }
+}
