014e07289f05466e5051ff4ccd27eb2998cbd894
Yasmeen Yousef <yasmeen.yousef05@gmail.com>
2023-04-05 16:08:28 -0400

    changes to displayMemes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const emotions = ["Adventurous", "Angry", "Disgusted", "Happy", "Sad", "Scared", "Sleepy"]
 const emotionsList = document.getElementById("emotions-list")
 const formDiv = document.getElementById("form-div")
+const baseUrl = 'http://localhost:3000/'
+const memesUrl = baseUrl + 'memes/'
 
 createEmotionsList(emotions)
 
@@ -40,24 +42,67 @@ function getMemes(emotion) {
 const memeDiv = document.getElementById("meme-div")
 
 function displayMemes(emotion){
+    const img = document.createElement("img")
+    const label = document.createElement("label")
+
+    const likesBtn = document.createElement("button")
+    likesBtn.textContent = "💓"
+    const dislikeBtn = document.createElement("button")
+    dislikeBtn.textContent = "💔"
+
+
+    let arr = []
 
     emotion.forEach(meme => {
-        const img = document.createElement("img")
-        img.src = meme.imageUrl
-        memeDiv.appendChild(img)
-    
-        const label = document.createElement("label")
-        label.textContent = `Likes: ${meme.likes}`
-        memeDiv.appendChild(label)
-    
-        const likesBtn = document.createElement("button")
-        likesBtn.textContent = " ♥ "
-        memeDiv.appendChild(likesBtn)
+        arr.push(meme)
     })
+
+//creating next and previous buttons
+
+    const nextBtn = document.createElement('button')
+    nextBtn.id = "next-button"
+    nextBtn.textContent = "NEXT"
+
+    const previousBtn = document.createElement("button")
+    nextBtn.id = "previous-button"
+    previousBtn.textContent = "Previous"
+
+
+ //placeholder image after clicking on button
+    img.src = arr[0].imageUrl
+    memeDiv.appendChild(img)
+    memeDiv.appendChild(previousBtn)
+
+    label.textContent = `Likes: ${arr[0].likes}`
+    memeDiv.appendChild(label)
+    
+    memeDiv.appendChild(likesBtn)
+    memeDiv.appendChild(dislikeBtn)
+    memeDiv.appendChild(nextBtn)
+
+//next button
+    let i = 0
+
+    nextBtn.addEventListener("click", ()=>{
+        i++
+        img.src = arr[i].imageUrl
+        label.textContent = `Likes: ${arr[i].likes}`
+    })
+
+    previousBtn.addEventListener("click", ()=>{
+        i--
+        img.src = arr[i].imageUrl
+        label.textContent = `Likes: ${arr[i].likes}`
+    })
+
+    console.log(arr[i])
+
+    dislikeBtn.addEventListener('click', () => decreaseMemeLikes(arr[i], label))
+
 }
 
 function addNewMemes(choice, imageUrl){
-    fetch("http:localhost:3000/memes", {
+    fetch(memesUrl, {
         method: "POST",
         headers: {
             "content-Type": "application/json"
@@ -83,4 +128,22 @@ function formEventListener(){
     })
 }
 
+function decreaseMemeLikes(meme, label) {
+
+    fetch(memesUrl + `${meme.id}`, {
+        method: 'PATCH',
+        headers: {
+            'content-type': 'application/json',
+            'accepts': 'application/json'
+        },
+        body: JSON.stringify({likes: meme.likes -= 1})
+        })
+    .then(res => res.json())
+    .then(patchedMemeData => {
+        label.textContent = `Likes: ${meme.likes}`
+    })
+}
+
 formEventListener()
+
+
