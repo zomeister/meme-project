7b4cd47c937d8c4ee21b96667787872624b63817
Yasmeen Yousef <yasmeen.yousef05@gmail.com>
2023-04-05 13:23:47 -0400

    post function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const emotions = ["Adventurous", "Angry", "Disgusted", "Happy", "Sad", "Scared", "Sleepy"]
 const emotionsList = document.getElementById("emotions-list")
+const formDiv = document.getElementById("form-div")
 
 createEmotionsList(emotions)
 
@@ -17,6 +18,7 @@ function createEmotionsList(emotions) {
         emotionsList.append(button)
         emotionOptionEventListener(button)
     })
+    formEventListener(emotions)
 }
 
 
@@ -32,23 +34,51 @@ function getMemes(emotion) {
     const emotionUrl = `http:localhost:3000/memes?feelingId=${emotion}`
     fetch(emotionUrl)
     .then(res=>res.json())
-    .then(memesData => memesData.forEach(meme => {
-        displayMemes(meme)
-    }))
+    .then(memesData => {
+        displayMemes(memesData)
+    })
 }
 const memeDiv = document.getElementById("meme-div")
 
 function displayMemes(emotion){
 
-    const img = document.createElement("img")
-    img.src = emotion.imageUrl
-    memeDiv.appendChild(img)
+    emotion.forEach(meme => {
+        const img = document.createElement("img")
+        img.src = meme.imageUrl
+        memeDiv.appendChild(img)
+    
+        const label = document.createElement("label")
+        label.textContent = `Likes: ${meme.likes}`
+        memeDiv.appendChild(label)
+    
+        const likesBtn = document.createElement("button")
+        likesBtn.textContent = " ♥ "
+        memeDiv.appendChild(likesBtn)
+    })
+}
 
-    const label = document.createElement("label")
-    label.textContent = `Likes: ${emotion.likes}`
-    memeDiv.appendChild(label)
+function addNewMemes(emotion,choice, imageUrl){
+    fetch("http:localhost:3000/memes", {
+        method: "POST",
+        headers: {
+            "content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "feelingId": choice,
+            "imageUrl": imageUrl,
+            "likes": 0
+        })
+    })
+    .then(res => res.json())
+    .then(data => getMemes(data))
+}
 
-    const likesBtn = document.createElement("button")
-    likesBtn.textContent = " ♥ "
-    memeDiv.appendChild(likesBtn)
+function formEventListener(emotion){
+    formDiv.addEventListener("submit", (e)=>{
+        const selectChoice = document.getElementById("select-choice").value
+        const imgUrl = document.getElementById("img-url").value
+        e.preventDefault()
+        addNewMemes(emotion,selectChoice,imgUrl)
+        formDiv.reset()
+    })
 }
